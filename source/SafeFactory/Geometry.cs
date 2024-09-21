c33067ea4cdc8f16dd99fa2afce3812c8bbfb77b
Ivan Olyanishin <56886020+IOExcept10n@users.noreply.github.com>
2024-09-21 22:02:58 +0500

    Working on logic implementation

---
+++ source/SafeFactory/Geometry.cs
@@ -0,0 +1,96 @@
+﻿// Copyright 2024 (c) MIDIFrogs (contact https://github.com/MIDIFrogs)
+// Distributed under AGPL v.3.0 license. See LICENSE.md file in the project root for more information
+using System;
+using SixLabors.ImageSharp;
+
+namespace SafeFactory
+{
+    public static class Geometry
+    {
+        public static bool IntersectsWith(this Rectangle rect, Point[] polygon)
+        {
+            // Check if any edge of the polygon intersects with the rectangle
+            for (int i = 0; i < polygon.Length; i++)
+            {
+                Point p1 = polygon[i];
+                Point p2 = polygon[(i + 1) % polygon.Length]; // Wrap around to the first point
+
+                // Check for intersection with rectangle edges
+                if (LinesIntersect(p1, p2, new Point(rect.Left, rect.Top), new Point(rect.Right, rect.Top)) ||
+                    LinesIntersect(p1, p2, new Point(rect.Right, rect.Top), new Point(rect.Right, rect.Bottom)) ||
+                    LinesIntersect(p1, p2, new Point(rect.Right, rect.Bottom), new Point(rect.Left, rect.Bottom)) ||
+                    LinesIntersect(p1, p2, new Point(rect.Left, rect.Bottom), new Point(rect.Left, rect.Top)))
+                {
+                    return true; // An edge intersects
+                }
+            }
+
+            // Check if the rectangle is completely inside the polygon
+            if (IsPointInPolygon(rect.Location, polygon) &&
+                IsPointInPolygon(new Point(rect.Right, rect.Top), polygon) &&
+                IsPointInPolygon(new Point(rect.Right, rect.Bottom), polygon) &&
+                IsPointInPolygon(new Point(rect.Left, rect.Bottom), polygon))
+            {
+                return true; // Rectangle is completely inside the polygon
+            }
+
+            // Check if any vertex of the polygon is inside the rectangle
+            foreach (var point in polygon)
+            {
+                if (rect.Contains(point))
+                {
+                    return true; // A vertex of the polygon is inside the rectangle
+                }
+            }
+
+            return false; // No intersection found
+        }
+
+        private static bool LinesIntersect(Point p1, Point p2, Point p3, Point p4)
+        {
+            int d1 = Direction(p3, p4, p1);
+            int d2 = Direction(p3, p4, p2);
+            int d3 = Direction(p1, p2, p3);
+            int d4 = Direction(p1, p2, p4);
+
+            // General case
+            if (d1 != d2 && d3 != d4)
+                return true;
+
+            // Special cases
+            return (d1 == 0 && OnSegment(p3, p4, p1)) ||
+                   (d2 == 0 && OnSegment(p3, p4, p2)) ||
+                   (d3 == 0 && OnSegment(p1, p2, p3)) ||
+                   (d4 == 0 && OnSegment(p1, p2, p4));
+        }
+
+        private static int Direction(Point p1, Point p2, Point p3)
+        {
+            int val = (p2.Y - p1.Y) * (p3.X - p2.X) - (p2.X - p1.X) * (p3.Y - p2.Y);
+            return (val == 0) ? 0 : (val > 0) ? 1 : 2; // collinear, clockwise, counterclockwise
+        }
+
+        private static bool OnSegment(Point p1, Point p2, Point p)
+        {
+            return p.X <= Math.Max(p1.X, p2.X) && p.X >= Math.Min(p1.X, p2.X) &&
+                   p.Y <= Math.Max(p1.Y, p2.Y) && p.Y >= Math.Min(p1.Y, p2.Y);
+        }
+
+        private static bool IsPointInPolygon(Point point, Point[] polygon)
+        {
+            int n = polygon.Length;
+            bool inside = false;
+
+            for (int i = 0, j = n - 1; i < n; j = i++)
+            {
+                if ((polygon[i].Y > point.Y) != (polygon[j].Y > point.Y) &&
+                    (point.X < (polygon[j].X - polygon[i].X) * (point.Y - polygon[i].Y) / (polygon[j].Y - polygon[i].Y) + polygon[i].X))
+                {
+                    inside = !inside;
+                }
+            }
+
+            return inside;
+        }
+    }
+}
--- source/SafeFactory/VideoCapture/FrameProcessor.cs
+++ source/SafeFactory/VideoCapture/FrameProcessor.cs
@@ -1,31 +1,123 @@
-﻿using Compunet.YoloV8;
+﻿// Copyright 2024 (c) MIDIFrogs (contact https://github.com/MIDIFrogs)
+// Distributed under AGPL v.3.0 license. See LICENSE.md file in the project root for more information
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Compunet.YoloV8;
 using Compunet.YoloV8.Data;
 using SixLabors.ImageSharp;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SafeFactory.VideoCapture
 {
-    internal class FrameProcessor
+    /// <summary>
+    /// Represents a service that performs asynchronous frame prediction.
+    /// </summary>
+    public class FrameProcessor
     {
-        private YoloPredictor predictor;
-        private YoloConfiguration configuration;
+        private readonly YoloPredictor boxPredictor;
+        private readonly YoloConfiguration configuration;
+        private readonly YoloPredictor posePredictor;
 
-        public FrameProcessor()
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FrameProcessor"/> class.
+        /// </summary>
+        /// <param name="boxPredictor">An instance of the <see cref="YoloPredictor"/> model trained for helmet and human head hitbox predictions.</param>
+        /// <param name="posePredictor">An instance of the <see cref="YoloPredictor"/> model trained for the human and robot poses prediction.</param>
+        /// <param name="configuration">Shared prediction parameters to customize the prediction.</param>
+        public FrameProcessor(YoloPredictor boxPredictor, YoloPredictor posePredictor, YoloConfiguration configuration)
         {
+            this.boxPredictor = boxPredictor;
+            this.posePredictor = posePredictor;
+            this.configuration = configuration;
         }
 
-        public async Task<YoloResult<Segmentation>> SegmentAsync(Image image)
+        /// <summary>
+        /// Compares two poses and calculates a normalized average distance between them.
+        /// </summary>
+        /// <remarks>
+        /// Note that this method should be called only on the same poses definitions.
+        /// </remarks>
+        /// <param name="pose1">The first pose to compare.</param>
+        /// <param name="pose2">The second pose to compare.</param>
+        /// <returns>A normalized average distance between the keypoints, ranging from 0 to 1.</returns>
+        public static float ComparePoses(Pose pose1, Pose pose2)
         {
-            return await predictor.SegmentAsync(image, configuration);
+            var kpPairs = from kp1 in pose1
+                          join kp2 in pose2
+                          on kp1.Index equals kp2.Index
+                          select new
+                          {
+                              P1 = kp1.Point,
+                              P2 = kp2.Point,
+                          };
+
+            float sum = 0;
+            float max = float.PositiveInfinity;
+            float min = float.NegativeInfinity;
+            int count = 0;
+
+            foreach (var pair in kpPairs)
+            {
+                float distance = PointDistance(pair.P1, pair.P2);
+                sum += distance;
+                count++;
+
+                max = MathF.Max(max, distance);
+                min = MathF.Min(min, distance);
+            }
+
+            if (count == 0)
+            {
+                return 0;
+            }
+
+            float average = sum / count;
+
+            if (max - min < 0.0001f)
+            {
+                return 0;
+            }
+
+            float normalizedAverage = (average - min) / (max - min);
+            return normalizedAverage;
+
+            static float PointDistance(Point p1, Point p2) =>
+                (p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y);
         }
 
-        public float CompareSegments(Segmentation previous, Segmentation next)
+        /// <summary>
+        /// Performs helmet and head detection asynchronously.
+        /// </summary>
+        /// <param name="image">An image frame to detect checked objects.</param>
+        /// <returns>Results of the detection to validate.</returns>
+        public async Task<YoloResult<Detection>> DetectAsync(Image image)
         {
-            throw new NotImplementedException();
+            return await boxPredictor.DetectAsync(image, configuration);
+        }
+
+        /// <summary>
+        /// Performs robot and humans pose detection asynchronously.
+        /// </summary>
+        /// <param name="image">An image frame to estimate poses.</param>
+        /// <returns>Results of the estimation to validate.</returns>
+        public async Task<YoloResult<Pose>> PoseAsync(Image image)
+        {
+            return await posePredictor.PoseAsync(image, configuration);
+        }
+
+        /// <summary>
+        /// Performs full frame processing asynchronously.
+        /// </summary>
+        /// <param name="frame">A frame to process.</param>
+        /// <param name="timestamp">Timestamp to save into a frame.</param>
+        /// <returns>Processed frame info to validate.</returns>
+        public async Task<FrameInfo> ProcessFrameAsync(Image frame, TimeSpan timestamp)
+        {
+            var boxTask = DetectAsync(frame);
+            var poseTask = PoseAsync(frame);
+            await Task.WhenAll(boxTask, poseTask);
+            // TODO: check the door.
+            return new(frame, timestamp, poseTask.Result, boxTask.Result, false);
         }
     }
 }

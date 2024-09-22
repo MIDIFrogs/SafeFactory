13a4aeb5c0700cf6386ed33e8b1b4f078452f04a
MaximOdincov <124523356+MaximOdincov@users.noreply.github.com>
2024-09-22 05:53:55 +0500

    Working on door tracking

---
+++ source/SafeFactory/VideoCapture/DoorBorderTracking.cs
@@ -0,0 +1,34 @@
+﻿using Emgu.CV;
+using Emgu.CV.Features2D;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using SixLabors.ImageSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SafeFactory.VideoCapture
+{
+    public static class DoorBorderTracking
+    {
+        public static List<Point> FindKeyPoints(Point x, Point y, int e)
+        {
+            List<Point> keypoints = [];
+            double dx = y.X - x.X;
+            double dy = y.Y - x.Y;
+            double angel = Math.Atan2(dy, dx);
+            do
+            {
+                keypoints.Add(new Point(x.X + e, y.Y + (int)(e * angel)));
+            } while (x.X + e <= y.X);
+
+            return keypoints;
+        }
+
+        public static bool IsDoorOpened(Image img, DoorBorder doorBorder)
+        {
+
+        }
+    }
+}
---
+++ source/SafeFactory/VideoCapture/LineMetric.cs
@@ -0,0 +1,52 @@
+﻿using Avalonia.Controls;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafeFactory.VideoCapture
+{
+    public class LineMetric
+    {
+        public Point Begin { get; set; }
+
+        public Point End { get; set; }
+
+        public Rgba32[] KeyColors { get; set; } = [];
+
+        public static IEnumerable<Point> FindKeyPoints(Point x, Point y, int step)
+        {
+            double dx = y.X - x.X;
+            double dy = y.Y - x.Y;
+            double angel = Math.Atan2(dy, dx);
+            do
+            {
+                yield return new Point(x.X + step, y.Y + (int)(step * angel));
+            } while (x.X + step <= y.X);
+        }
+
+        public static LineMetric Compute(Point begin, Point end, Image<Rgba32> img)
+        {
+            List<Rgba32> keyColors = [];
+            foreach (var point in FindKeyPoints(begin, end, 1))
+            {
+                keyColors.Add(img[point.X, point.Y]);
+            }
+            return new()
+            {
+                Begin = begin,
+                End = end,
+                KeyColors = [.. keyColors]
+            };
+        }
+
+        public bool Compare(LineMetric other)
+        {
+            if (Begin != other.Begin) 
+                // Throw
+        }
+    }
+}
